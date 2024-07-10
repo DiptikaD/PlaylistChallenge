789b77e7b3c3fa95f1a2482297c0b44e7df2948d
Devi <diptikadevi@gmail.com>
2024-07-10 17:14:41 -0400

    finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,40 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int goBackward = 0;
+        int goForward = 0;
+
+        for (int i = startIndex; i >= 0; i--){
+            if (playList[i].equals(selection)){
+                System.out.println(goBackward);
+                break;
+            }
+            if (i==0){
+                i = playList.length;
+            }
+            goBackward++;
+        }
+
+        for (int i = startIndex; i <= playList.length; i++){
+            if (i == playList.length){
+                i = 0;
+            }
+            if (playList[i].equals(selection)){
+                System.out.println(goForward);
+                break;
+
+            }
+            goForward++;
+        }
+
+        if (goBackward < goForward){
+            return goBackward;
+        } else return goForward;
+    }
+
+    public static void main(String[] args) {
+        String[] playlist = {"wheniseeyouagain","borntorun","nothingelsematters","cecelia"};
+        Music music = new Music(playlist);
+        music.selection(2, "cecelia");
     }
 }
